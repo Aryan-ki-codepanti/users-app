6e745dbbf6a4506fb361668e700b54346cdb6faa
Aryan Sethi <aryan.rdps@gmail.com>
2021-11-25 18:51:31 +0530

    Added Styles to home heading and a google font

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,34 +1,36 @@
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux';
-import { ProfileCard } from '../../Components/ProfileCard/ProfileCard';
-import { fetchAsyncUsers } from '../../redux'
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { ProfileCard } from "../../Components/ProfileCard/ProfileCard";
+import { fetchAsyncUsers } from "../../redux";
+import "./Home.css";
 
-const Home = ({ status , fetchUsers }) => {
-
+const Home = ({ status, fetchUsers }) => {
     useEffect(() => {
         status.users.length === 0 && fetchUsers();
-    } , [ fetchUsers , status.users.length ]);
+    }, [fetchUsers, status.users.length]);
 
     return (
-        <div>
-            Home Page
+        <div className="container">
+            <h1 className="heading"> Top Users </h1>
+            <h2> Status : {status.loading ? "Loading" : "Not Loading"} </h2>
             
-            <h2> Status : { status.loading ? "Loading" : "Not Loading" } </h2>
-            {status.users.map(user => (
-                <div className="profile" key={user.id}>
-                    <ProfileCard user={user} />
-                </div>
-            ))}
+            <div className="profile-container">
+                {status.users.map(user => (
+                    <div className="profile" key={user.id}>
+                        <ProfileCard user={user} />
+                    </div>
+                ))}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 const mapStateToProps = state => ({
-    status: state.users   
+    status: state.users
 });
 
 const mapDispatchToProps = dispatch => ({
-    fetchUsers : () => dispatch(fetchAsyncUsers())
+    fetchUsers: () => dispatch(fetchAsyncUsers())
 });
 
-export default connect(mapStateToProps,mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
